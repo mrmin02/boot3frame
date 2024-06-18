6c190d4b92a52d1817b6632d5415178eb289ac4b
cms <mrmin02@naver.com>
2024-06-18 16:02:32 +0900

    feat: 배너 관리기능 추가

--- src/main/java/com/custom/boot3Cms/application/mng/banner/control/BannerMngController.java
+++ src/main/java/com/custom/boot3Cms/application/mng/banner/control/BannerMngController.java
@@ -7,7 +7,9 @@ import com.custom.boot3Cms.application.common.utils.StringUtil;
 import com.custom.boot3Cms.application.mng.banner.service.BannerMngService;
 import com.custom.boot3Cms.application.mng.banner.vo.BannerMngVO;
 import com.custom.boot3Cms.application.common.system.file.service.FileMngService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -54,6 +56,7 @@ public class BannerMngController {
      * @return
      * @throws Exception
      */
+    @Operation(summary = "배너 목록")
     @GetMapping(value = "/api/mng/banner/list")
     public ResultVO bannerList(@ModelAttribute("BannerMngVO") BannerMngVO bannerMngVO,
                             HttpServletRequest request,
@@ -72,7 +75,7 @@ public class BannerMngController {
             e.printStackTrace();
             result = false;
             code = 404;
-            rMsg = "회원 수정 중 오류가 발생하였습니다.";
+            rMsg = "배너 목록 조회 중 오류가 발생하였습니다.";
         }
 
         resultVO.setResultMsg(rMsg);
@@ -83,70 +86,138 @@ public class BannerMngController {
 
     /**
      * 배너 상세보기
-     * @param bannerMngVO
      * @param request
      * @param response
-     * @param model
-     * @param session
-     * @param redirectAttributes
      * @param principal
      * @return
      * @throws Exception
      */
-    @RequestMapping(value = "/mng/banner/detail/{banner_seq}")
-    public String bannerDetail(@ModelAttribute("bannerMngVO") BannerMngVO bannerMngVO,
-                                @PathVariable("banner_seq") String banner_seq,
+    @Operation(summary = "배너 상세정보")
+    @GetMapping(value = "/api/mng/banner/detail/{banner_seq}")
+    public ResultVO bannerDetail(@PathVariable("banner_seq") String banner_seq,
                                 HttpServletRequest request,
                                 HttpServletResponse response,
-                                ModelMap model,
-                                HttpSession session,
-                                RedirectAttributes redirectAttributes,
                                 Principal principal) throws Exception {
-        Map<String, Object> map = bannerMngService.getBannerDetail(new BannerMngVO(banner_seq));
-        if((boolean)map.get("result")){
-            model.addAttribute("bannerMngVO", map.get("value"));
-        }else{
-            redirectAttributes.addFlashAttribute("rHeader",map.get("rHeader"));
-            redirectAttributes.addFlashAttribute("rMsg",map.get("rMsg"));
-            return "redirect:/mng/banner/list";
+        ResultVO resultVO = new ResultVO();
+        boolean result = false;
+        int code = 200;
+        String rMsg = "";
+
+        try{
+            Map<String, Object> map = bannerMngService.getBannerDetail(new BannerMngVO(banner_seq));
+            if((boolean)map.get("result")){
+                resultVO.putResult("data",map.get("value"));
+                result = true;
+            }else{
+                rMsg = String.valueOf(map.get("rMsg"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            result = false;
+            code = 404;
+            rMsg = "배너 상세정보 조회 중 오류가 발생하였습니다.";
         }
-        return "mng/banner/detail";
+
+        resultVO.setResultMsg(rMsg);
+        resultVO.putResult("result",result);
+        resultVO.setResultCode(code);
+        return resultVO;
     }
 
+
     /**
-     * 배너 등록/수정 FORM
+     * 배너 등록/수정/삭제
      * @param bannerMngVO
      * @param request
      * @param response
-     * @param model
-     * @param session
-     * @param redirectAttributes
      * @param principal
      * @return
      * @throws Exception
      */
-    @RequestMapping(value = "/mng/banner/form")
-    public String bannerForm(@ModelAttribute("bannerMngVO") BannerMngVO bannerMngVO,
-                               HttpServletRequest request,
-                               HttpServletResponse response,
-                               ModelMap model,
-                               HttpSession session,
-                               RedirectAttributes redirectAttributes,
-                               Principal principal) throws Exception {
-        if(StringUtil.isNotEmpty(bannerMngVO.getBanner_seq())){
-            Map<String, Object> map = bannerMngService.getBannerDetail(new BannerMngVO(bannerMngVO.getBanner_seq()));
-            if((boolean)map.get("result")){
-                BannerMngVO vo = (BannerMngVO) map.get("value");
-                if(vo != null){
-                    model.addAttribute("bannerMngVO", vo);
-                }
-            }else{
-                redirectAttributes.addFlashAttribute("rHeader",map.get("rHeader"));
-                redirectAttributes.addFlashAttribute("rMsg",map.get("rMsg"));
-                return "redirect:/mng/banner/list";
+    @Operation(summary = "배너 등록")
+    @PostMapping(value = "/api/mng/banner/proc" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResultVO bannerInsertProc(@ModelAttribute("bannerMngVO") BannerMngVO bannerMngVO,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  Principal principal) throws Exception {
+        ResultVO resultVO = new ResultVO();
+        boolean result = false;
+        int code = 200;
+        String rMsg = "";
+
+        try{
+            if(principal != null){
+                LoginVO vo = CommonUtil.fn_getUserAuth(principal);
+                bannerMngVO.setInpt_seq(vo.getUser_seq());
+                bannerMngVO.setUpd_seq(vo.getUser_seq());
             }
+            bannerMngVO.setFlag("c");
+            Map<String, Object> map = bannerMngService.bannerProc(bannerMngVO);
+            if((boolean)map.get("result")){
+                resultVO.putResult("data",map.get("value"));
+                result = true;
+            }else{
+                rMsg = String.valueOf(map.get("rMsg"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            result = false;
+            code = 404;
+            rMsg = "배너 등록 중 오류가 발생하였습니다.";
         }
-        return "mng/banner/form";
+
+        resultVO.setResultMsg(rMsg);
+        resultVO.putResult("result",result);
+        resultVO.setResultCode(code);
+        return resultVO;
+    }
+
+
+    /**
+     * 배너 등록/수정/삭제
+     * @param bannerMngVO
+     * @param request
+     * @param response
+     * @param principal
+     * @return
+     * @throws Exception
+     */
+    @Operation(summary = "배너 수정")
+    @PutMapping(value = "/api/mng/banner/proc" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResultVO bannerUpdateProc(@ModelAttribute("bannerMngVO") BannerMngVO bannerMngVO,
+                                     HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     Principal principal) throws Exception {
+        ResultVO resultVO = new ResultVO();
+        boolean result = false;
+        int code = 200;
+        String rMsg = "";
+
+        try{
+            if(principal != null){
+                LoginVO vo = CommonUtil.fn_getUserAuth(principal);
+                bannerMngVO.setInpt_seq(vo.getUser_seq());
+                bannerMngVO.setUpd_seq(vo.getUser_seq());
+            }
+            bannerMngVO.setFlag("u");
+            Map<String, Object> map = bannerMngService.bannerProc(bannerMngVO);
+            if((boolean)map.get("result")){
+                resultVO.putResult("data",map.get("value"));
+                result = true;
+            }else{
+                rMsg = String.valueOf(map.get("rMsg"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            result = false;
+            code = 404;
+            rMsg = "배너 수정 중 오류가 발생하였습니다.";
+        }
+
+        resultVO.setResultMsg(rMsg);
+        resultVO.putResult("result",result);
+        resultVO.setResultCode(code);
+        return resultVO;
     }
 
     /**
@@ -154,55 +225,46 @@ public class BannerMngController {
      * @param bannerMngVO
      * @param request
      * @param response
-     * @param model
-     * @param session
-     * @param redirectAttributes
      * @param principal
      * @return
      * @throws Exception
      */
-    @RequestMapping(value = "/mng/banner/proc")
-    public String bannerProc(@ModelAttribute("bannerMngVO") BannerMngVO bannerMngVO,
-                                  HttpServletRequest request,
-                                  HttpServletResponse response,
-                                  ModelMap model,
-                                  HttpSession session,
-                                  RedirectAttributes redirectAttributes,
-                                  Principal principal) throws Exception {
-        if(principal != null){
-            LoginVO vo = CommonUtil.fn_getUserAuth(principal);
-            bannerMngVO.setInpt_seq(vo.getUser_seq());
-            bannerMngVO.setUpd_seq(vo.getUser_seq());
-        }
-        Map<String, Object> map = bannerMngService.bannerProc(bannerMngVO);
-        String rtnUrl = "";
-        if((boolean)map.get("result")){
-            if("d".equals(bannerMngVO.getFlag())){
-                rtnUrl = "redirect:/mng/banner/list";
-            }else{
-                rtnUrl = "redirect:/mng/banner/detail/"+bannerMngVO.getBanner_seq();
+    @Operation(summary = "배너 삭제")
+    @DeleteMapping(value = "/api/mng/banner/proc")
+    public ResultVO bannerDeleteProc(@RequestBody BannerMngVO bannerMngVO,
+                                     HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     Principal principal) throws Exception {
+        ResultVO resultVO = new ResultVO();
+        boolean result = false;
+        int code = 200;
+        String rMsg = "";
+
+        try{
+            if(principal != null){
+                LoginVO vo = CommonUtil.fn_getUserAuth(principal);
+                bannerMngVO.setInpt_seq(vo.getUser_seq());
+                bannerMngVO.setUpd_seq(vo.getUser_seq());
             }
-            redirectAttributes.addFlashAttribute("rHeader", map.get("rHeader"));
-            redirectAttributes.addFlashAttribute("rMsg", map.get("rMsg"));
-        }else{
-            if("d".equals(bannerMngVO.getFlag())){
-                redirectAttributes.addFlashAttribute("rHeader", map.get("rHeader"));
-                redirectAttributes.addFlashAttribute("rMsg", map.get("rMsg"));
-                rtnUrl = "redirect:/mng/banner/detail/"+bannerMngVO.getBanner_seq();
-            }else if("c".equals(bannerMngVO.getFlag()) || "u".equals(bannerMngVO.getFlag())){
-                model.addAttribute("rHeader", map.get("rHeader"));
-                model.addAttribute("rMsg", map.get("rMsg"));
-                model.addAttribute("bannerMngVO", bannerMngVO);
-                rtnUrl = "mng/banner/form";
+            bannerMngVO.setFlag("d");
+            Map<String, Object> map = bannerMngService.bannerProc(bannerMngVO);
+            if((boolean)map.get("result")){
+                resultVO.putResult("data",map.get("value"));
+                result = true;
             }else{
-                redirectAttributes.addFlashAttribute("rHeader", map.get("rHeader"));
-                redirectAttributes.addFlashAttribute("rMsg", map.get("rMsg"));
-                rtnUrl = "redirect:/mng/banner/list";
+                rMsg = String.valueOf(map.get("rMsg"));
             }
+        }catch (Exception e){
+            e.printStackTrace();
+            result = false;
+            code = 404;
+            rMsg = "배너 삭제 중 중 오류가 발생하였습니다.";
         }
-        return rtnUrl;
+
+        resultVO.setResultMsg(rMsg);
+        resultVO.putResult("result",result);
+        resultVO.setResultCode(code);
+        return resultVO;
     }
-
-
 
 }
